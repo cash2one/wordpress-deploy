056eb84296692ad9a494d44cce51b554ffe63c5d
David Simic <david@moringasci.com>
2016-01-31 15:03:43 -0500

    mods to install  backup plugin and setenv

--- README.md
+++ README.md
@@ -4,26 +4,27 @@ wordpress-deploy
 Fabric scripts for the semi-automated install and setup of a fresh wordpress 
 install served by nginx using fastcgi.
 
-Usage:
+Install core dependencies on server:
 
-    fab -H user@host setup_core db_setup setup_wordpress setup_nginx
-
+    fab -H user@host setup_core
 
 To fresh install mysql:
 
-    fab -H user@host --set db_root_pwd=ROOT_PASS install_mysql
+    fab -H user@host setenv:db_root_pwd=DBROOTPWD install_mysql
 
 To setup wordpress site mysql db:
 
-    fab -H user@host --set db_root_pwd=ROOT_PASS,db_name=SITENAME,\
-        db_user=NAME,db_user_pwd=DB_USER_PASS db_setup
+    fab -H angel@do-small-3 setenv:db_name=DBNAME,db_root_pwd=DBROOTPWD\
+        db_user=DBUSER,db_user_pwd=DBPWD db_setup
 
-Wordpress + nginx setup
+Wordpress + nginx setup:
 
-    fab -H user@host --set site_name=SITENAME \
-        setup_wordpress setup_nginx
-
+    fab -H angel@do-small-3 setenv:db_name=DBNAME,\
+        db_user=DBUSER,db_user_pwd=DBPWD,site_name=SITENAME,\
+        nginx_root=NGINX_ROOT setup_wordpress setup_nginx
+        
+with NGINX_ROOT set to where the config files are contained, typically "/etc/nginx/"
 
 To install the dropbox backup plugin:
     
-    fab -H user@host install_plugin_dropbox_backup
+    fab -H user@host setenv:site_name=SITENAME install_plugin_dropbox_backup
--- fabfile.py
+++ fabfile.py
@@ -23,10 +23,10 @@ env.db_root_pwd = None
 env.TMP_DIR = "/var/tmp/wordpress"
 
 # Wordpress Constants.
-env.WORDPRESS_DWNLD_LINK = "wget http://wordpress.org/latest.tar.gz"
+env.WORDPRESS_DWNLD_LINK = "http://wordpress.org/latest.tar.gz"
 
 # Nginx
-env.NGINX_ROOT = "/var/lib/tomcat7"
+env.nginx_root = "/etc/nginx/"
 
 PKG_SETUP_CMD = \
     """
@@ -63,6 +63,7 @@ def setup_core():
     sudo(PKG_SETUP_CMD.format("nginx"))
     sudo(PKG_SETUP_CMD.format("php5-gd libssh2-php"))
     sudo(PKG_SETUP_CMD.format("php5-fpm"))
+    sudo(PKG_SETUP_CMD.format("php5-mysqlnd-ms"))
 
 
 def setup_php_fpm():
@@ -78,19 +79,20 @@ def setup_nginx():
     """
     put(
         "./nginx/sites-available/site_name",
-        "%s/sites-available/" % env.NGINX_ROOT, use_sudo=True)
+        "%s/sites-available/" % env.nginx_root, use_sudo=True)
     sudo(
         """
         cd {0}/sites-available;
         sed  -i 's/\@\@SITENAME\@\@/{1}/g' site_name
         mv {0}/sites-available/site_name {0}/sites-available/{1}
         """.format(
-            env.NGINX_ROOT, env.site_name
+            env.nginx_root, env.site_name
         )
     )
-    if not exists('%s/sites-enabled/%s' % (env.NGINX_ROOT, env.site_name)):
+    if not exists('%s/sites-enabled/%s' % (env.nginx_root, env.site_name)):
         sudo('ln -s {0}/sites-available/{1} {0}/sites-enabled/{1}'.
-             format(env.NGINX_ROOT, env.site_name))
+             format(env.nginx_root, env.site_name))
+    sudo("service nginx restart")
 
 
 def install_mysql():
@@ -124,22 +126,28 @@ def db_purge(usr, pwd, db_name):
     )
 
 
-def db_setup():
+def db_setup(
+):
     """
     Sets up
     """
     run(
         """
-        yes | mysql -u {0} -p"{1}" -e"CREATE DATABASE {2}"
+        yes | mysql -u {0} -p"{1}" -e"CREATE DATABASE IF NOT EXISTS {2}"
+
         mysql -u {0} -p"{1}" -e\
-            "CREATE USER {3}@localhost IDENTIFIED BY '{4}'"
-        mysql -u {0} -p"{1}" -e\
-            "GRANT ALL PRIVILEGES ON {2}.* {3}@localhost"
+            "GRANT ALL PRIVILEGES ON {2}.* TO {3}@localhost identified by '{4}'"
         mysql -u {0} -p"{1}" -e"FLUSH PRIVILEGES"
         """.format(
             "root", env.db_root_pwd, env.db_name, env.db_user,
             env.db_user_pwd),
     )
+
+
+def setenv(**kwargs):
+    for key, val in kwargs.items():
+        print "Setting env var %s to %s" % (key, val)
+        env[key] = val
 
 
 def setup_wordpress():
@@ -148,6 +156,7 @@ def setup_wordpress():
     """
     # Teardown and recreate tmp dir.
     run("rm -rf {0}; mkdir -p {0}".format(env.TMP_DIR))
+    assert env.site_name
     with cd(env.TMP_DIR):
         run("wget {0}".format(env.WORDPRESS_DWNLD_LINK))
         run("tar xzvf latest.tar.gz")
@@ -157,16 +166,17 @@ def setup_wordpress():
             cp wp-config-sample.php wp-config.php
             sed  -i 's/database_name_here/{0}/g' wp-config.php
             sed  -i 's/username_here/{1}/g' wp-config.php
-            sed  -i 's/password_here/{1}/g' wp-config.php
+            sed  -i 's/password_here/{2}/g' wp-config.php
             """.format(
                 env.db_name, env.db_user, env.db_user_pwd
             )
         )
-        sudo("cp -rf wordpress /var/www/%s" % env.site_name)
-        sudo("chown -R www-data /var/www/%s" % env.site_name)
+        sudo("mkdir -p /var/www/%s" % env.site_name)
+        sudo("cp -rf wordpress/* /var/www/%s" % env.site_name)
+        sudo("chown -R www-data:www-data /var/www/%s" % env.site_name)
 
 
-def setup_plugin_dropbox_backup():
+def install_plugin_dropbox_backup():
     """
     Automates the install and server setup for the
     dropbox back up plugin.
@@ -175,11 +185,16 @@ def setup_plugin_dropbox_backup():
     download_link = "https://downloads.wordpress.org/plugin/" +\
         "%s.zip" % plugin_name
     # Install needed packages if not present.
-    sudo(PKG_SETUP_CMD.format("curl libcurl3 libcurl3-dev php5-curl"))
+    sudo(PKG_SETUP_CMD.format("curl"))
+    sudo(PKG_SETUP_CMD.format("libcurl3"))
+    sudo(PKG_SETUP_CMD.format("libcurl3-dev"))
+    sudo(PKG_SETUP_CMD.format("php5-curl"))
     with cd(env.TMP_DIR):
+        run("rm -rf %s*" % plugin_name)
         run("wget %s" % download_link)
         run("unzip %s.zip" % plugin_name)
-        sudo("mkdir -p /var/www/{1}/wp-content/backups")
+        sudo("mkdir -p /var/www/%s/wp-content/backups" % env.site_name)
+        sudo("chown www-data /var/www/%s/wp-content/backups" % env.site_name)
         sudo("cp -rf {0} /var/www/{1}/wp-content/plugins".format(
             plugin_name, env.site_name)
         )
